2649c1aa5bdeb4328e73be2d1a6c7090fdb9e022
Tawhid Jony <tawhid.jony.jhm@gmail.com>
2021-09-18 18:35:10 +0600

    new update

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -4,6 +4,7 @@ import {motion} from 'framer-motion'
 import {RiHomeGearLine} from 'react-icons/ri'
 import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
 import {MdContactPhone} from 'react-icons/md'
+
 const Layout = ({children}) => {
     return (
         <motion.div 
@@ -22,9 +23,9 @@ const Layout = ({children}) => {
             {children}
             <nav className="absolute bottom-0 transform left-2/4 -translate-x-2/4 ">
                 <ul className="flex ">
-                    <li className="flex items-center justify-center w-10 h-10 p-4 m-2 mb-4 text-lg text-white transition duration-300 ease-out transform bg-white border-2 rounded-full shadow border-charleston-100 hover:scale-125 bg-opacity-20 bg-clip-padding backdrop-filter"><Link to="/" ><RiHomeGearLine size={20} color={'#fff'} /></Link></li>
-                    <li className="flex items-center justify-center w-10 h-10 p-4 m-2 mb-4 text-lg text-white transition duration-300 ease-out transform bg-white border-2 rounded-full shadow border-charleston-100 hover:scale-125 bg-opacity-20 bg-clip-padding backdrop-filter"><Link to="/project" ><AiOutlineFundProjectionScreen size={20} color={'#fff'}/></Link></li>
-                    <li className="flex items-center justify-center w-10 h-10 p-4 m-2 mb-4 text-lg text-white transition duration-300 ease-out transform bg-white border-2 rounded-full shadow border-charleston-100 hover:scale-125 bg-opacity-20 bg-clip-padding backdrop-filter"><Link to="/contact" ><MdContactPhone size={20} color={'#fff'}/></Link></li>
+                    <Link to="/"        className="flex items-center justify-center w-10 h-10 p-2 m-2 mb-4 text-lg text-white transition duration-300 ease-out transform bg-white border-2 rounded-full shadow border-charleston-100 hover:scale-125 bg-opacity-20 bg-clip-padding backdrop-filter"><RiHomeGearLine size={50} color={'#fff'} /></Link>
+                    <Link to="/project" className="flex items-center justify-center w-10 h-10 p-2 m-2 mb-4 text-lg text-white transition duration-300 ease-out transform bg-white border-2 rounded-full shadow border-charleston-100 hover:scale-125 bg-opacity-20 bg-clip-padding backdrop-filter"><AiOutlineFundProjectionScreen size={20} color={'#fff'}/></Link>
+                    <Link to="/contact" className="flex items-center justify-center w-10 h-10 p-2 m-2 mb-4 text-lg text-white transition duration-300 ease-out transform bg-white border-2 rounded-full shadow border-charleston-100 hover:scale-125 bg-opacity-20 bg-clip-padding backdrop-filter"><MdContactPhone size={20} color={'#fff'}/></Link>
                 </ul>
             </nav>
         </motion.div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import {motion} from 'framer-motion'
-import Layout from '../components/layouts/Layout'
-import { container } from '../animation/Variants'
-
+import loadable from '@loadable/component'
+import profileImg from '../assets/images/profile/we.webp'
+const Layout     = loadable(() => import('../components/layouts/Layout'))
+const container  = loadable(() => import('../animation/Variants'))
 
 const HomePage = () => {
+
+    const shareData = {
+        title: 'MDN',
+        text: 'Learn web development on MDN!',
+        url: 'https://developer.mozilla.org'
+      }
+      
+    const shareSocial = (e) =>{
+        if (navigator.share) {
+            navigator
+              .share(shareData)
+              .then(() => {
+                console.log('Successfully shared');
+              })
+              .catch(error => {
+                console.error('Something went wrong sharing the blog', error);
+              });
+        }
+
+        console.log(navigator.share);
+    }
+
     return (
         <Layout>
            <motion.div 
@@ -12,10 +35,11 @@ const HomePage = () => {
                 initial="hidden"
                 animate="visible"
                 className="flex flex-col items-center justify-center h-full">
-                    <img className="w-40 h-40 rounded-full" src="https://cdn2.vectorstock.com/i/thumb-large/17/61/male-avatar-profile-picture-vector-10211761.jpg" alt="" />
+                    <img className="w-40 h-40 rounded-full" src={profileImg} alt="" />
                     <h2 className="mt-4 text-xl text-charleston-100">MD TAWHIDUR RAHMAN</h2>
                     <h3 className="mt-2 text-sm uppercase text-charleston-100">associate software engineer</h3>
            </motion.div>
+           <button id="shareBtn" onClick={shareSocial} > Share</button>
         </Layout>
     )
 }
